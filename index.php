14417be855f466e6271835f0377934a9a558c7a7
Marcos de Melo <marcdmelo@gmail.com>
2024-08-27 14:39:43 -0300

    Aula 02 - Pagina Home - aplicando bootstrap

--- index.php
+++ index.php
@@ -10,19 +10,44 @@ include_once("./db/conexao.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilo.css">
     <title>LOCADORA DEV</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <header>
-        <h1>LOCADORA DEV</h1>
-        <ul>
-            <li><a href="index.php?menu=home">Home</a></li>
-            <li><a href="index.php?menu=videos">Vídeos</a></li>
-            <li><a href="index.php?menu=categorias">Categorias</a></li>
-            <li><a href="index.php?menu=clientes">Clientes</a></li>
-            <li><a href="index.php?menu=locacao">Locação</a></li>
-        </ul>
+    <header class="container-fluid bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="index.php" class="navbar-brand bi bi-camera-reels-fill">
+                    LOCADORA DEV
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center">
+                        <li class="nav-item"><a
+                                class="nav-link bi bi-house <?php echo ($menu == 'home') ? 'active' : '' ?>"
+                                href="index.php?menu=home"> Home</a></li>
+                        <li class="nav-item"><a
+                                class="nav-link bi bi-film <?php echo ($menu == 'videos') ? 'active' : '' ?>"
+                                href="index.php?menu=videos"> Vídeos</a></li>
+                        <li class="nav-item"><a
+                                class="nav-link bi bi-bookmark-plus <?php echo ($menu == 'categorias') ? 'active' : '' ?>"
+                                href="index.php?menu=categorias"> Categorias</a></li>
+                        <li class="nav-item"><a
+                                class="nav-link bi bi-people <?php echo ($menu == 'clientes') ? 'active' : '' ?>"
+                                href="index.php?menu=clientes"> Clientes</a></li>
+                        <li class="nav-item"><a
+                                class="nav-link bi bi-bag-plus <?php echo ($menu == 'locacao') ? 'active' : '' ?>"
+                                href="index.php?menu=locacao"> Locação</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </header>
     <main>
         <!-- Conteúdo principal do sistema  -->
@@ -104,6 +129,9 @@ include_once("./db/conexao.php");
         ?>
         <!-- ----------------------------- -->
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
