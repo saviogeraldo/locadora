96a739a9bcdadea494f237b16b1514414853ded0
Marcos de Melo <marcdmelo@gmail.com>
2024-08-28 11:55:14 -0300

    Aula 05 - Pagina Inserir Vídeo

--- cad-videos.php
+++ cad-videos.php
@@ -1,36 +1,61 @@
-<h2>Cadastro de Videos</h2>
+<div class="container">
+    <h2 class="bi bi-film"><i class="bi bi-plus"></i> Cadastro de Videos</h2>
 
-<form action="index.php?menu=inserir-videos" method="post">
-    <div>
-        <label for="tituloFilme">Título do Vídeo</label>
-        <input type="text" name="tituloFilme" id="tituloFilme" required>
-    </div>
-    <div>
-        <label for="duracaoFilme">Duração do Vídeo</label>
-        <input type="text" name="duracaoFilme" id="duracaoFilme" required>
-    </div>
-    <div>
-        <label for="valorLocacao">Valor da Locação</label>
-        <input type="text" name="valorLocacao" id="valorLocacao" required>
-    </div>
-    <div>
-        <label for="idCategoria">Categoria</label>
-        <select name="idCategoria" id="idCategoria" required>
-            <option value="">Selecione a categoria</option>
-            <?php
-            $sql = "SELECT * FROM tbcategorias order by nomeCategoria ASC";
-            $rs = mysqli_query($conexao, $sql);
-            while ($dados = mysqli_fetch_assoc($rs)) {
+    <form action="index.php?menu=inserir-videos" method="post">
+        <div class="mb-3 col-6">
+            <label class="form-label" for="tituloFilme">Título do Vídeo</label>
+            <div class="input-group">
+                <div class="input-group-text">
+                    <i class="bi bi-film"></i>
+                </div>
+                <input class="form-control" type="text" name="tituloFilme" id="tituloFilme" required>
+            </div>
+        </div>
+        <div class="mb-3 col-6">
+            <label class="form-label" for="duracaoFilme">Duração do Vídeo</label>
+            <div class="input-group">
+                <div class="input-group-text">
+                    <i class="bi bi-clock"></i>
+                </div>
+                <input class="form-control" type="text" name="duracaoFilme" id="duracaoFilme" required>
+            </div>
+        </div>
+        <div class="mb-3 col-3">
+            <label class="form-label" for="valorLocacao">Valor da Locação</label>
+            <div class="input-group">
+                <div class="input-group-text">
+                    <i class="bi bi-cash-coin"></i>
+                </div>
+                <input class="form-control" type="text" name="valorLocacao" id="valorLocacao" required>
+            </div>
+        </div>
+        <div class="mb-3 col-3">
+            <label class="form-label" for="idCategoria">Categoria</label>
+            <div class="input-group">
+                <div class="input-group-text">
+                    <i class="bi bi-bookmark-plus"></i>
+                </div>
+                <select class="form-select" name="idCategoria" id="idCategoria" required>
+                    <option value="">Selecione a categoria</option>
+                    <?php
+                    $sql = "SELECT * FROM tbcategorias order by nomeCategoria ASC";
+                    $rs = mysqli_query($conexao, $sql);
+                    while ($dados = mysqli_fetch_assoc($rs)) {
 
-            ?>
-                <option value="<?= $dados["idCategoria"] ?>"><?= $dados["nomeCategoria"] ?></option>
-            <?php
-            }
-            ?>
-        </select>
-
-    </div>
-    <div>
-        <button type="submit">Salvar</button>
-    </div>
-</form>
+                        ?>
+                        <option value="<?= $dados["idCategoria"] ?>">
+                            <?= $dados["nomeCategoria"] ?>
+                        </option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="mb-3">
+            <button class="btn btn-primary" type="submit">
+                <i class="ph-duotone ph-floppy-disk"></i> Salvar
+            </button>
+        </div>
+    </form>
+</div>
--- inserir-videos.php
+++ inserir-videos.php
@@ -1,12 +1,13 @@
-<h2>Inserir Vídeo</h2>
+<div class="container">
+    <h2 class="bi bi-film"><i class="bi bi-plus"></i> Cadastro de Videos</h2>
 
-<?php
-$tituloFilme = $_POST["tituloFilme"];
-$duracaoFilme = $_POST["duracaoFilme"];
-$valorLocacao = $_POST["valorLocacao"];
-$idCategoria = $_POST["idCategoria"];
+    <?php
+    $tituloFilme = $_POST["tituloFilme"];
+    $duracaoFilme = $_POST["duracaoFilme"];
+    $valorLocacao = $_POST["valorLocacao"];
+    $idCategoria = $_POST["idCategoria"];
 
-$sql = "INSERT INTO tbfilmes (
+    $sql = "INSERT INTO tbfilmes (
     tituloFilme,
     duracaoFilme,
     valorLocacao,
@@ -19,11 +20,22 @@ $sql = "INSERT INTO tbfilmes (
     '$idCategoria'
     )
     ";
-    $rs = mysqli_query($conexao,$sql);
+    $rs = mysqli_query($conexao, $sql);
 
-    if($rs){
-        echo "<p>Registro inserido com sucesso</p>";
-    }else{
-        echo "<p>Erro ao inserir</p>";
+    if ($rs) {
+        ?>
+        <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Adicionando novo registro</h4>
+            <p>Registro inserido com sucesso!</p>
+            <p>Clique <a href="index.php?menu=videos" class="alert-link">aqui</a> para a lista de vídeos.</p>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div class="alert alert-danger" role="alert">
+            <p>Erro ao inserir um novo registro.</p>
+        </div>
+        <?php
     }
-?>
+    ?>
+</div>
