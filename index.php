6ec2500780009d28b6849e0cad536540a2dcabf8
saviogeraldo <146675666+saviogeraldo@users.noreply.github.com>
2024-09-10 19:46:18 -0300

    Voltei titulo

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include_once("./db/conexao.php");
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a href="index.php" class="navbar-brand bi bi-camera-reels-fill">
-                    LOCADORA Savio
+                    LOCADORA DEV
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
