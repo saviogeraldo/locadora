8aee492ef9418b1bcd7d99f71f9e1b0ab9a720b4
savio <saviogeraldo85@gmail.com>
2024-09-16 22:07:11 -0300

    inserir botstrap lista-clientes

--- lista-clientes.php
+++ lista-clientes.php
@@ -1,10 +1,11 @@
-<h2>Lista de Clientes</h2>
-<div>
-    <a href="index.php?menu=cad-clientes">Cadastrar nova Cliente</a>
+<h2 class="text-center my-4">Lista de Clientes</h2>
+
+<div class="text-center mb-3">
+    <a href="index.php?menu=cad-clientes" class="btn btn-primary">Cadastrar novo Cliente</a>
 </div>
-<div>
+
+<div class="mb-4">
     <?php
-    //$txtPesquisa = (isset($_POST["txtPesquisa"]))?$_POST["txtPesquisa"]:"";
     if (isset($_POST["txtPesquisa"])) {
         $txtPesquisa = $_POST["txtPesquisa"];
     } else {
@@ -12,19 +13,20 @@
     }
     ?>
 
-    <form action="" method="post">
-        <label for="txtPesquisa">Pesquisa</label>
-        <input type="search" name="txtPesquisa" id="txtPesquisa" value="<?=$txtPesquisa?>">
-        <button type="submit">
-            OK
-        </button>
+    <form action="" method="post" class="form-inline">
+        <div class="form-group">
+            <label for="txtPesquisa" class="mr-2">Pesquisa</label>
+            <input type="search" name="txtPesquisa" id="txtPesquisa" value="<?=$txtPesquisa?>" class="form-control mr-2">
+            <button type="submit" class="btn btn-success">OK</button>
+        </div>
     </form>
 </div>
-<table border="1">
-    <thead>
+
+<table class="table table-striped">
+    <thead class="thead-dark">
         <tr>
             <th>id</th>
-            <th>Nome da Cliente</th>
+            <th>Nome do Cliente</th>
             <th>Telefone</th>
             <th>E-Mail</th>
             <th>Editar</th>
@@ -32,9 +34,8 @@
         </tr>
     </thead>
     <tbody>
-    <?php
-        $sql = "SELECT * FROM tbclientes 
-        where nomeCliente like '%{$txtPesquisa}%'";
+        <?php
+        $sql = "SELECT * FROM tbclientes WHERE nomeCliente like '%{$txtPesquisa}%'";
         $rs = mysqli_query($conexao, $sql);
         while ($dados = mysqli_fetch_assoc($rs)) {
         ?>
@@ -43,12 +44,11 @@
                 <td><?= $dados["nomeCliente"] ?></td>
                 <td><?= $dados["telefoneCliente"] ?></td>
                 <td><?= $dados["emailCliente"] ?></td>
-                <td><a href="index.php?menu=editar-clientes&idCliente=<?=$dados["idCliente"]?>">Editar</a></td>
-                <td><a href="index.php?menu=excluir-clientes&idCliente=<?=$dados["idCliente"]?>">excluir</a></td>
+                <td><a href="index.php?menu=editar-clientes&idCliente=<?=$dados["idCliente"]?>" class="btn btn-warning">Editar</a></td>
+                <td><a href="index.php?menu=excluir-clientes&idCliente=<?=$dados["idCliente"]?>" class="btn btn-danger">Excluir</a></td>
             </tr>
         <?php
         }
         ?>
     </tbody>
-
 </table>
