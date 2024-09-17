b0398413ad265b4b51bd700cbfd66ed140ba9fe2
savio <saviogeraldo85@gmail.com>
2024-09-16 21:58:37 -0300

    inserir botstrap atl-cat

--- atualizar-categorias.php
+++ atualizar-categorias.php
@@ -1,18 +1,15 @@
-<h2>Atualizar Categoria</h2>
+<h2 class="text-center my-4">Atualizar Categoria</h2>
+
 <?php
 $idCategoria = $_POST["idCategoria"];
 $nomeCategoria = $_POST["nomeCategoria"];
 
-
-$sql = "update tbCategorias set 
-nomeCategoria='{$nomeCategoria}'
-
-where idCategoria = '{$idCategoria}'
-";
-
+$sql = "UPDATE tbcategorias SET nomeCategoria='{$nomeCategoria}' WHERE idCategoria = '{$idCategoria}'";
 $rs = mysqli_query($conexao,$sql);
 
-echo "<p>Registro atualizado com sucesso!</p>";
-
-
+if($rs){
+    echo "<p class='alert alert-success'>Registro atualizado com sucesso!</p>";
+} else {
+    echo "<p class='alert alert-danger'>Erro ao atualizar o registro.</p>";
+}
 ?>
