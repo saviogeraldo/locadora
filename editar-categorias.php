92994777523058ef2463ce6c4e57d64ca2b96ce6
savio <saviogeraldo85@gmail.com>
2024-09-16 22:02:16 -0300

    inserir botstrap edt-categoria

--- editar-categorias.php
+++ editar-categorias.php
@@ -1,23 +1,23 @@
+<h2 class="text-center my-4">Editar Categoria</h2>
+
 <?php
 $idCategoria = $_GET["idCategoria"];
-$sql = "SELECT * FROM tbCategorias WHERE idCategoria = '{$idCategoria}'";
+$sql = "SELECT * FROM tbcategorias WHERE idCategoria = '{$idCategoria}'";
 $rs = mysqli_query($conexao, $sql) or die("Erro ao realizar a consulta. Erro: " . mysqli_error($conexao));
 $dados = mysqli_fetch_assoc($rs);
 ?>
 
-<h2>Editar categoria</h2>
-
-<form action="index.php?menu=atualizar-categorias" method="post">
-    <div>
+<form action="index.php?menu=atualizar-categorias" method="post" class="container">
+    <div class="form-group">
         <label for="idCategoria">ID</label>
-        <input type="text" name="idCategoria" id="idCategoria" value="<?=$dados["idCategoria"]?>" readonly>
+        <input type="text" name="idCategoria" id="idCategoria" value="<?=$dados["idCategoria"]?>" class="form-control" readonly>
     </div>
-    <div>
+    <div class="form-group">
         <label for="nomeCategoria">Nome da Categoria</label>
-        <input type="text" name="nomeCategoria" id="nomeCategoria" value="<?=$dados["nomeCategoria"]?>">
+        <input type="text" name="nomeCategoria" id="nomeCategoria" value="<?=$dados["nomeCategoria"]?>" class="form-control">
     </div>
   
-    <div>
-         <input type="submit" value="Salvar">   
+    <div class="text-center">
+         <input type="submit" value="Salvar" class="btn btn-primary">   
     </div>
 </form>
