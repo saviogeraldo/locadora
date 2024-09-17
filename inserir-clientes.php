4c03c51c536f6a9cf3b84fe4694ab30d1063d53a
savio <saviogeraldo85@gmail.com>
2024-09-16 22:05:53 -0300

    inserir botstrap inser-clientes

--- inserir-clientes.php
+++ inserir-clientes.php
@@ -1,9 +1,9 @@
-<h2>Inserir Cliente</h2>
+<h2 class="text-center my-4">Inserir Cliente</h2>
+
 <?php
 $nomeCliente = $_POST["nomeCliente"];
 $telefoneCliente = $_POST["telefoneCliente"];
 $emailCliente = $_POST["emailCliente"];
-
 
 $sql = "INSERT INTO tbClientes (
     nomeCliente,
@@ -16,11 +16,11 @@ $sql = "INSERT INTO tbClientes (
     '$emailCliente'
     )
     ";
-    $rs = mysqli_query($conexao,$sql);
+$rs = mysqli_query($conexao, $sql);
 
-    if($rs){
-        echo "<p>Registro inserido com sucesso</p>";
-    }else{
-        echo "<p>Erro ao inserir</p>";
-    }
+if($rs){
+    echo "<p class='alert alert-success'>Registro inserido com sucesso</p>";
+}else{
+    echo "<p class='alert alert-danger'>Erro ao inserir o registro</p>";
+}
 ?>
