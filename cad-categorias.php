a1317261422c8ec809d5735b662967b585f38b30
savio <saviogeraldo85@gmail.com>
2024-09-16 22:00:22 -0300

    inserir botstrap cad-categ

--- cad-categorias.php
+++ cad-categorias.php
@@ -1,12 +1,12 @@
-<h2>Cadastro de nova categoria</h2>
+<h2 class="text-center my-4">Cadastro de nova Categoria</h2>
 
-<form action="index.php?menu=inserir-categorias" method="post">
-    <div>
+<form action="index.php?menu=inserir-categorias" method="post" class="container">
+    <div class="form-group">
         <label for="nomeCategoria">Nome da Categoria</label>
-        <input type="text" name="nomeCategoria" id="nomeCategoria">
+        <input type="text" name="nomeCategoria" id="nomeCategoria" class="form-control">
     </div>
-  
-    <div>
-         <input type="submit" value="Salvar">   
+
+    <div class="text-center">
+         <input type="submit" value="Salvar" class="btn btn-primary">   
     </div>
 </form>
