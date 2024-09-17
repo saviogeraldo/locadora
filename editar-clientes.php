18edb70720447ce80b9daac9c0fb504edd1d08be
savio <saviogeraldo85@gmail.com>
2024-09-16 22:02:51 -0300

    inserir botstrap edt-cliente

--- editar-clientes.php
+++ editar-clientes.php
@@ -1,3 +1,5 @@
+<h2 class="text-center my-4">Editar Cliente</h2>
+
 <?php
 $idCliente = $_GET["idCliente"];
 $sql = "SELECT * FROM tbclientes WHERE idCliente = '{$idCliente}'";
@@ -5,29 +7,25 @@ $rs = mysqli_query($conexao, $sql) or die("Erro ao realizar a consulta. Erro: " 
 $dados = mysqli_fetch_assoc($rs);
 ?>
 
-<h2>Editar Cliente</h2>
-
-<form action="index.php?menu=atualizar-clientes" method="post">
-    <div>
+<form action="index.php?menu=atualizar-clientes" method="post" class="container">
+    <div class="form-group">
         <label for="idCliente">ID</label>
-        <input type="text" name="idCliente" id="idCliente" value="<?=$dados["idCliente"]?>" readonly>
+        <input type="text" name="idCliente" id="idCliente" value="<?=$dados["idCliente"]?>" class="form-control" readonly>
     </div>
-    <div>
+    <div class="form-group">
         <label for="nomeCliente">Nome do Cliente</label>
-        <input type="text" name="nomeCliente" id="nomeCliente" value="<?=$dados["nomeCliente"]?>">
+        <input type="text" name="nomeCliente" id="nomeCliente" value="<?=$dados["nomeCliente"]?>" class="form-control">
     </div>
-  
-    <div>
+    <div class="form-group">
         <label for="telefoneCliente">Telefone</label>
-        <input type="text" name="telefoneCliente" id="telefoneCliente" value="<?=$dados["telefoneCliente"]?>">
+        <input type="text" name="telefoneCliente" id="telefoneCliente" value="<?=$dados["telefoneCliente"]?>" class="form-control">
     </div>
-  
-    <div>
+    <div class="form-group">
         <label for="emailCliente">E-Mail</label>
-        <input type="email" name="emailCliente" id="emailCliente" value="<?=$dados["emailCliente"]?>">
+        <input type="email" name="emailCliente" id="emailCliente" value="<?=$dados["emailCliente"]?>" class="form-control">
     </div>
   
-    <div>
-         <input type="submit" value="Salvar">   
+    <div class="text-center">
+         <input type="submit" value="Salvar" class="btn btn-primary">   
     </div>
 </form>
