3e4b4493e031dd53de8702bcfab06da25f07a5ef
savio <saviogeraldo85@gmail.com>
2024-09-16 21:59:28 -0300

    inserir botstrap atl-clients

--- atualizar-clientes.php
+++ atualizar-clientes.php
@@ -1,10 +1,10 @@
-<h2>Atualizar Cliente</h2>
+<h2 class="text-center my-4">Atualizar Cliente</h2>
+
 <?php
 $idCliente = $_POST["idCliente"];
 $nomeCliente = $_POST["nomeCliente"];
 $telefoneCliente = $_POST["telefoneCliente"];
 $emailCliente = $_POST["emailCliente"];
-
 
 $sql = "update tbClientes set 
 nomeCliente='{$nomeCliente}',
@@ -16,7 +16,9 @@ where idCliente = '{$idCliente}'
 
 $rs = mysqli_query($conexao,$sql);
 
-echo "<p>Registro atualizado com sucesso!</p>";
-
-
+if($rs){
+    echo "<p class='alert alert-success'>Registro atualizado com sucesso!</p>";
+}else{
+    echo "<p class='alert alert-danger'>Erro ao atualizar o registro.</p>";
+}
 ?>
