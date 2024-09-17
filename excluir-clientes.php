8f90d0afe6d03490ca47d342a40eedf552d71c1b
savio <saviogeraldo85@gmail.com>
2024-09-16 22:04:25 -0300

    inserir botstrap excl-clientes

--- excluir-clientes.php
+++ excluir-clientes.php
@@ -1,9 +1,13 @@
-<h2>Excluir Cliente</h2>
+<h2 class="text-center my-4">Excluir Cliente</h2>
+
 <?php
 $idCliente = $_GET["idCliente"];
-$sql = "delete from tbClientes where idCliente = '{$idCliente}'";
-$rs = mysqli_query($conexao,$sql);
+$sql = "delete from tbclientes where idCliente = '{$idCliente}'";
+$rs = mysqli_query($conexao, $sql);
 
-echo "<p>Registro excluido com sucesso!</p>";
-
+if ($rs) {
+    echo "<p class='alert alert-success'>Registro excluído com sucesso!</p>";
+} else {
+    echo "<p class='alert alert-danger'>Erro ao excluir o registro.</p>";
+}
 ?>
