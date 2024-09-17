a27230d0088539b8ca5aa76595e008ab214a576f
savio <saviogeraldo85@gmail.com>
2024-09-16 22:01:08 -0300

    inserir botstrap cad-clientes

--- cad-clientes.php
+++ cad-clientes.php
@@ -1,20 +1,20 @@
-<h2>Cadastro de novo Cliente</h2>
+<h2 class="text-center my-4">Cadastro de novo Cliente</h2>
 
-<form action="index.php?menu=inserir-clientes" method="post">
-    <div>
-        <label for="nomeCliente">Nome da Cliente</label>
-        <input type="text" name="nomeCliente" id="nomeCliente">
+<form action="index.php?menu=inserir-clientes" method="post" class="container">
+    <div class="form-group">
+        <label for="nomeCliente">Nome do Cliente</label>
+        <input type="text" name="nomeCliente" id="nomeCliente" class="form-control">
     </div>
-    <div>
+    <div class="form-group">
         <label for="telefoneCliente">Telefone</label>
-        <input type="text" name="telefoneCliente" id="telefoneCliente">
+        <input type="text" name="telefoneCliente" id="telefoneCliente" class="form-control">
     </div>
-    <div>
+    <div class="form-group">
         <label for="emailCliente">E-Mail</label>
-        <input type="email" name="emailCliente" id="emailCliente">
+        <input type="email" name="emailCliente" id="emailCliente" class="form-control">
     </div>
   
-    <div>
-         <input type="submit" value="Salvar">   
+    <div class="text-center">
+         <input type="submit" value="Salvar" class="btn btn-primary">   
     </div>
 </form>
